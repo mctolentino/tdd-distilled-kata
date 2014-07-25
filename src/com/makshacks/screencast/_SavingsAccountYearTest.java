27106ad21561829519093c2390b5ba14ce3bf4ac
Maki Tolentino <maki.tolentino.03@gmail.com>
2014-07-25 21:27:35 +0800

    End of Chapter 2.

--- src/com/makshacks/screencast/_SavingsAccountYearTest.java
+++ src/com/makshacks/screencast/_SavingsAccountYearTest.java
@@ -18,66 +18,93 @@ public class _SavingsAccountYearTest {
 		
 		@Test
 		public void endingBalance(){
-			assertEquals(11000, newAccount().endingBalance());
+			assertEquals(11000, newAccount().endingBalance(25));
 		}
 		
 		@Test
 		public void nextYearStartingBalanceEqualsThisYearsEndingBalance(){
 			SavingsAccountYear account = newAccount();
-			assertEquals(account.endingBalance(), newAccount().nextYear().startingBalance());
+			assertEquals(account.endingBalance(25), newAccount().nextYear(25).startingBalance());
 		}
 		
 		@Test
 		public void nextYearsInterestRateEqualsThisYearsInterestRate(){
 			SavingsAccountYear thisYear = newAccount();
-			assertEquals(thisYear.interestRate(), thisYear.nextYear().interestRate());
+			assertEquals(thisYear.interestRate(), thisYear.nextYear(25).interestRate());
 		}
 		
 		@Test
 		public void withdrawingFundsOccursAtTheBeginningOfTheYear(){
 			SavingsAccountYear year = new SavingsAccountYear(10000, 10);
 			year.withdraw(1000);
-			assertEquals(9900, year.endingBalance());
+			assertEquals(9900, year.endingBalance(25));
 		}
 		
 		@Test
 		public void startingPrincipal(){
-			SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
 			assertEquals("starting principal", 3000, year.startingPrincipal());
 		}
 		
 		@Test
 		public void endingPrincipal(){
-			SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
-			assertEquals("starting principal", 3000, year.startingPrincipal());
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
 			year.withdraw(2000);
 			assertEquals("ending principal", 1000, year.endingPrincipal());
 		}
 		
 		@Test
 		public void endingPrincipalNeverGoesBelowZero(){
-			SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
-			assertEquals("starting principal", 3000, year.startingPrincipal());
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
 			year.withdraw(4000);
 			assertEquals("ending principal",  0, year.endingPrincipal());
 		}
 		
 		@Test
 		public void multipleWithdrawalsInAYear(){
-			// TODO
+			SavingsAccountYear year = new SavingsAccountYear(10000, 10);
+			year.withdraw(1000);
+			year.withdraw(2000);
+			assertEquals(3000, year.totalWithdrawn());
 		}
 		
+		@Test
+		public void capitalGainsWithdrawn() {
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+ 			year.withdraw(1000);
+			assertEquals(0, year.capitalGainsWithdrawn());
+			year.withdraw(3000);
+			assertEquals(1000, year.capitalGainsWithdrawn());
+		}
 		
+		@Test
+		public void capitalGainsTaxIncurred(){
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+	 		year.withdraw(5000);
+	 		assertEquals(2000, year.capitalGainsWithdrawn());
+	 		assertEquals(666, year.capitalGainsTaxIncurred(25));
+		}
 		
-//		@Test
-//		public void withdrawingMoreThanPrincipalIncursCapitalGainsTax() {
-//			SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
-//			year.withdraw(3000);
-//			assertEquals(7700, year.endingBalance());
-//			year.withdraw(5000);
-//			assertEquals(2000+200- (5000 *.25), year.endingBalance()); 
-//		}
-
+		@Test
+		public void capitalGainsTaxIncurred_NeedsToCoverCapitalGainsWithdrawn_AND_theAdditionalCapitalGainsWithdrawnToPayCapitalGainsTax(){
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+			year.withdraw(5000);
+			assertEquals(2000, year.capitalGainsWithdrawn());
+			assertEquals(666, year.capitalGainsTaxIncurred(25));
+			
+		}
+		
+		@Test
+		public void capitalGainsTaxIsIncludedInEndingBalance(){
+			SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+	 		year.withdraw(5000);
+	 		assertEquals(2000, year.capitalGainsWithdrawn());
+	 		assertEquals(666, year.capitalGainsTaxIncurred(25));
+	 		int expectedStartingBalanceAfterWithdrawals = 10000 - 5000 - 666;
+	 		assertEquals((int)(expectedStartingBalanceAfterWithdrawals * 1.10), year.endingBalance(25));
+			
+		}
+		
 		private SavingsAccountYear newAccount() {
 			SavingsAccountYear account = new SavingsAccountYear(10000, 10);
 			return account;
